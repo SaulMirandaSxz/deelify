2c3ce37764ea3e13b79313a8c553fcdcd15a1a38
Saul Miranda <36282163+SaulMirandaSxz@users.noreply.github.com>
2022-09-05 10:39:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/api/expenses', async (req, res) => {
 })
 
 // Listen
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 app.listen(port, () => 
   console.log(`Listening on port ${port}...`)
 )
